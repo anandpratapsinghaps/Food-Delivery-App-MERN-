0a7a116e7aca902ea02ae8fd90120ae648d6ac27
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-04-13 20:52:57 +0530

    last commit

--- my-app/src/Screens/Home.js
+++ my-app/src/Screens/Home.js
@@ -3,6 +3,7 @@ import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Card from '../Components/Card'
 
+
 export default function Home() {
     const [foodCat, setFoodCat] = useState([])
     const [foodItems, setFoodItems] = useState([])
--- my-app/src/Screens/MyOrder.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MyOrder() {
-return (
-    <div>
-        
-    </div>
-)
-}
