4d03459770e803b43cec88f2e7b3084e3a4be97a
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2024-11-15 01:24:43 +0530

    last commit

--- my-app/backend/db.js
+++ my-app/backend/db.js
@@ -1,30 +1,23 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://anandpratapsingh04:food_alix001@cluster0.n3s1e0j.mongodb.net/FoodDeliveryApp(MERN)?retryWrites=true&w=majority&appName=Cluster0'
-const mongoDB =async()=>{
-    await mongoose.connect(mongoURI,{useNewUrlParser:true},async (err,result)=>{
-        if(err) console.log("Error",err)
-        else{
-            console.log('connected');
-            const fetched_data = await mongoose.connection.db.collection("food_items");
-            fetched_data.find({}).toArray(async function(err,data){
-            const foodCollection = await mongoose.connection.db.collection("food_category"); 
-            foodCollection.find({}).toArray(async function(err,catdata){
-                if(err) console.log(err);
-                else{
-                    global.food_items = data;
-                    global.food_category = catdata;
-                }
-            })  
-                // if(err) console.log(err);
-                // else{
-                //     global.food_items = data;
-                // }
-            })
-    }
-    });
-}
+const mongoURI = 'mongodb+srv://anandpratapsingh04:food_alix001@cluster0.n3s1e0j.mongodb.net/FoodDeliveryApp(MERN)?retryWrites=true&w=majority&appName=Cluster0';
 
+const mongoDB = async () => {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to MongoDB');
 
-module.exports=mongoDB;
+    // Fetch food items and food categories
+    const fetched_data = await mongoose.connection.db.collection("food_items").find({}).toArray();
+    const foodCollection = await mongoose.connection.db.collection("food_category").find({}).toArray();
 
+    // Store data globally
+    global.food_items = fetched_data;
+    global.food_category = foodCollection;
 
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = mongoDB;
